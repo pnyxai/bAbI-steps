1a4174422a724eb48c6657a79d0516ab095f74ec
Nicolas Aguirre <nicolasaguirre90@hotmail.com>
2025-07-21 04:05:18 -0300

    fix immediate options' for `none`.

--- babisteps/basemodels/complextracking.py
+++ babisteps/basemodels/complextracking.py
@@ -9,7 +9,7 @@ from babisteps import operators
 from babisteps.basemodels import groups as gr
 from babisteps.basemodels.FOL import FOL, Exists, From, FromTo, In, Out, To
 from babisteps.basemodels.generators import (
-    DELIM, BaseGenerator, OBJECTS_LOCATION_EVENT_NONE_ANSWERS, UNKNONW_ANSWERS)
+    DELIM, OBJECTS_LOCATION_EVENT_NONE_ANSWERS, UNKNONW_ANSWERS, BaseGenerator)
 from babisteps.basemodels.nodes import (Coordenate, Entity,
                                         ObjectInLocationState,
                                         ObjectInLocationStatePolar, State)
